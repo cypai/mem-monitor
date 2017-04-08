61e93eecf0c9e2b1ca1f6ff12cd044d7d6e4b5d2
Charlie Pai <cpai@fandango.com>
2017-04-07 23:17:26 -0700

    Add notifications

--- mem-monitor.sh
+++ mem-monitor.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-THRESHOLD=50
+THRESHOLD=90
+USED_PERCENTAGE=`free | grep Mem | awk '{print $3/$2 * 100.0}'`
 FREE_PERCENTAGE=`free | grep Mem | awk '{print $4/$2 * 100.0}'`
 
 if (( $(echo "$FREE_PERCENTAGE < $THRESHOLD" | bc -l) )); then
-    echo "Memory usage is high! Free percentage: $FREE_PERCENTAGE"
+    echo "Memory usage is high!"
+	echo "Used percentage: $USED_PERCENTAGE"
+	echo "Free percentage: $FREE_PERCENTAGE"
     ps aux  | awk '{print $6/1024 " MB\t\t" $11}'  | sort -nr | head -n10
+    exit 1
 else
-    echo "Memory is OK. Free percentage: $FREE_PERCENTAGE"
+    echo "Memory is OK."
+	echo "Used percentage: $USED_PERCENTAGE"
+	echo "Free percentage: $FREE_PERCENTAGE"
+    exit 0
 fi
---
+++ notify.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+OUTPUT=$($SCRIPT_DIR/mem-monitor.sh)
+CODE=$?
+
+if [ $CODE -eq 1 ]; then
+    notify-send --urgency=critical "Memory monitor alert" "$OUTPUT"
+fi
