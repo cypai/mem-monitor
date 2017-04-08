2bfbb080317f873d621a3ca6e2001776c29d8f45
Charlie Pai <cpai@fandango.com>
2017-04-07 23:27:38 -0700

    Fix indentation

--- mem-monitor.sh
+++ mem-monitor.sh
@@ -6,13 +6,13 @@ FREE_PERCENTAGE=`free | grep Mem | awk '{print $4/$2 * 100.0}'`
 
 if (( $(echo "$FREE_PERCENTAGE < $THRESHOLD" | bc -l) )); then
     echo "Memory usage is high!"
-	echo "Used percentage: $USED_PERCENTAGE"
-	echo "Free percentage: $FREE_PERCENTAGE"
+    echo "Used percentage: $USED_PERCENTAGE"
+    echo "Free percentage: $FREE_PERCENTAGE"
     ps aux  | awk '{print $6/1024 " MB\t\t" $11}'  | sort -nr | head -n10
     exit 1
 else
     echo "Memory is OK."
-	echo "Used percentage: $USED_PERCENTAGE"
-	echo "Free percentage: $FREE_PERCENTAGE"
+    echo "Used percentage: $USED_PERCENTAGE"
+    echo "Free percentage: $FREE_PERCENTAGE"
     exit 0
 fi
